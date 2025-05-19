2c75015fec2e93e1b1ef2bd7c67f0fa7a22ec6dc
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-05-19 18:32:43 -0300

    refactor: implementei o modelo binomial completo de precificação de opções (CRR) comk um payoff genérico.

--- src/chapter3/binomial_option_value.py
+++ src/chapter3/binomial_option_value.py
@@ -1,32 +1,53 @@
 import math
+from typing import Callable
 
 
-def option_value(
-    r: float,
-    delta_t: int,
+def binomial_option_price(
+    asset: float,
     sigma: float,
-    v_positive: float,
-    v_negative: float,
+    rate: float,
+    strike: float,
+    expiry: float,
+    numb_steps: int,
+    payoff: Callable[[float, float], float],
 ) -> float:
 
-    u = 1 + sigma * math.sqrt(delta_t)
-    v = 1 - sigma * math.sqrt(delta_t)
+    dt = expiry / numb_steps
+    discount_factor = math.exp(-rate * dt)
 
-    p = 1 / 2 + (r * math.sqrt(delta_t)) / (2 * sigma)
+    u = math.exp(sigma * math.sqrt(dt))
+    d = 1 / u
+    p = (math.exp(rate * dt) - d) / (u - d)
 
-    expected_value = p * v_positive + (1 - p) * v_negative
+    S = [0.0 for _ in range(numb_steps + 1)]
+    S[0] = asset
 
-    option_value = expected_value / (1 + r * delta_t)
+    for n in range(1, numb_steps + 1):
+        for j in reversed(range(1, n + 1)):
+            S[j] = u * S[j - 1]
+        S[0] = d * S[0]
 
-    return option_value
+    V = [payoff(S[j], strike) for j in range(numb_steps + 1)]
+
+    for n in reversed(range(1, numb_steps + 1)):
+        for j in range(n):
+            V[j] = (p * V[j + 1] + (1 - p) * V[j]) * discount_factor
+
+    return V[0]
+
+
+def call_payoff(S, K):
+    return max(S - K, 0)
 
 
 if __name__ == "__main__":
-    s = 100
-    r = 0.1
-    delta_t = 1 / 12  # 1 / 252
-    sigma = 0.2
-    v_positive = 12.0
-    v_negative = 5.0
-    value = option_value(r, delta_t, sigma, v_positive, v_negative)
-    print(f"{value:.2f}")
+    price = binomial_option_price(
+        asset=100,
+        sigma=0.2,
+        rate=0.05,
+        strike=100,
+        expiry=1,
+        numb_steps=3,
+        payoff=call_payoff,
+    )
+    print(f"Option Price: {price:.4f}")
